9698b22804cf262d9fcd9035573e25769c2bf232
Aran Leite <hyoretsu@gmail.com>
2020-07-16 19:01:32 -0300

    Edited various configs
    ESLint's, Babel's, Jest's, TypeScript's, .gitignore's

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,16 @@
 module.exports = {
- presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'],
+ presets: [
+  [
+   '@babel/preset-env',
+   {
+    targets: {
+     node: 'current',
+    },
+   },
+  ],
+  '@babel/preset-typescript',
+ ],
+ ignore: ['**/*.spec.ts'],
  plugins: [
   [
    'module-resolver',
