fbaed07a2e63987c12fcbd70450aa3929ca174ae
Aran Leite <hyoretsu@gmail.com>
2021-06-29 02:52:18 -0300

    Updated configs

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,20 @@
-const { pathsToModuleNameMapper } = require('ts-jest/utils');
-const { compilerOptions } = require('./tsconfig.json');
+/* eslint-disable import/no-extraneous-dependencies */
+import type { Config } from '@jest/types';
+import { pathsToModuleNameMapper } from 'ts-jest/utils';
 
-module.exports = {
+import { compilerOptions } from './tsconfig.json';
+
+const config: Config.InitialOptions = {
  clearMocks: true,
  collectCoverage: true,
  collectCoverageFrom: ['<rootDir>/src/modules/**/services/*.ts'],
  coverageDirectory: 'coverage',
  coverageReporters: ['text-summary', 'lcov'],
+ moduleFileExtensions: ['ts'],
  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/src/' }),
  preset: 'ts-jest',
  testEnvironment: 'node',
  testMatch: ['**/*.spec.ts'],
 };
+
+export default config;
